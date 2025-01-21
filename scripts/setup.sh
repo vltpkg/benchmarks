32dc0329e002a7a461b7fb458445312f28b20756
Darcy Clarke <darcy@darcyclarke.me>
2025-01-21 02:47:50 +0000

    fix: remove remote caching

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,6 @@
 COREPACK_ENABLE_STRICT=0
 COREPACK_ENABLE_AUTO_PIN=0
 YARN_ENABLE_IMMUTABLE_INSTALLS=false
-NX_CLOUD_ACCESS_TOKEN="${{ secrets.NX_TOKEN }}"
-TURBO_TOKEN="${{ secrets.TURBO_TOKEN }}"
-TURBO_TEAMID="${{ secrets.TURBO_TEAMID }}"
 
 # Install Package Managers & Benchmark Tools
 cargo install hyperfine --quiet
@@ -32,8 +29,3 @@ echo "bun: $BUN_VERSION"
 echo "deno: $DENO_VERSION"
 
 echo "{ npm: \"$NPM_VERSION\", vlt: \"$VLT_VERSION\", yarn: \"$YARN_VERSION\", berry: \"$BERRY_VERSION\", pnpm: \"$PNPM_VERSION\", bun: \"$BUN_VERSION\", deno: \"$DENO_VERSION\" }" > ./results/versions.json
-
-# Log Benchmark Configurations
-echo "COREPACK_ENABLE_STRICT: ${{ env.COREPACK_ENABLE_STRICT }}"
-echo "COREPACK_ENABLE_AUTO_PIN: ${{ env.COREPACK_ENABLE_AUTO_PIN }}"
-echo "YARN_ENABLE_IMMUTABLE_INSTALLS: ${{ env.YARN_ENABLE_IMMUTABLE_INSTALLS }}"
