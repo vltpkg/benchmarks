bb18cf49154ac59710c2b80821dcf8b7d9ff974b
darcyclarke <darcy@darcyclarke.me>
2025-03-31 12:46:05 -0400

    fix: remove binary specific logic for vlt

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,22 +8,17 @@ YARN_ENABLE_IMMUTABLE_INSTALLS=false
 # Install Package Managers & Benchmark Tools
 apt-get install jq
 cargo install hyperfine --quiet
-npm install -g npm@latest corepack@latest vlt@latest bun@latest deno@latest nx@latest turbo@latest # install all other tools
+npm install -g npm@latest vlt@latest corepack@latest vlt@latest bun@latest deno@latest nx@latest turbo@latest # install all other tools
 
 # Configure Package Managers
 corepack enable yarn pnpm # enable yarn & pnpm via corepack (as is their preferred method)
 npm config set loglevel silent # make npm silent
 
-# Get the compiled vlt, rename to blt & copy all bins into a cache directory
-curl "$(npm view @vltpkg/cli-linux-x64@latest dist.tarball)" | tar -xzv
-./package/vlt --help
-
-# rename to blt
-mkdir -p ./bins
-mv ./package/vlt ./bins/blt
-
-cp "$(which jq)" "$(which hyperfine)" "$(which npm)" "$(which corepack)" "$(which vlt)" "$(which bun)" "$(which deno)" "$(which nx)" "$(which turbo)" ./bins
-echo "$PWD/bins" >> "$GITHUB_PATH"
+# # Copy all bins into a cache directory
+# TODO: Add this back in once we have a deterministic way to cache the bins
+# mkdir -p ./bins
+# cp "$(which jq)" "$(which hyperfine)" "$(which npm)" "$(which corepack)" "$(which vlt)" "$(which bun)" "$(which deno)" "$(which nx)" "$(which turbo)" ./bins
+# echo "$PWD/bins" >> "$GITHUB_PATH"
 
 # Create Results Directory
 mkdir -p ./results/
@@ -31,7 +26,6 @@ mkdir -p ./results/
 # Log Package Manager Versions
 NPM_VERSION="$(npm -v)"
 VLT_VERSION="$(vlt -v)"
-BLT_VERSION="$(blt -v)"
 YARN_VERSION="$(corepack yarn@1 -v)"
 BERRY_VERSION="$(corepack yarn@latest -v)"
 PNPM_VERSION="$(corepack pnpm@latest -v)"
@@ -43,7 +37,6 @@ NODE_VERSION=$(node -e "console.log(process.version.substr(1))")
 
 echo "npm: $NPM_VERSION"
 echo "vlt: $VLT_VERSION"
-echo "blt: $BLT_VERSION"
 echo "yarn: $YARN_VERSION"
 echo "berry: $BERRY_VERSION"
 echo "pnpm: $PNPM_VERSION"
@@ -53,4 +46,4 @@ echo "nx: $NX_VERSION"
 echo "turbo: $TURBO_VERSION"
 echo "node: $NODE_VERSION"
 
-echo "{ npm: \"$NPM_VERSION\", vlt: \"$VLT_VERSION\", blt: \"$BLT_VERSION\", yarn: \"$YARN_VERSION\", berry: \"$BERRY_VERSION\", pnpm: \"$PNPM_VERSION\", bun: \"$BUN_VERSION\", deno: \"$DENO_VERSION\", nx: \"$NX_VERSION\", turbo: \"$TURBO_VERSION\", node: \"$NODE_VERSION\" }" > ./results/versions.json
+echo "{ npm: \"$NPM_VERSION\", vlt: \"$VLT_VERSION\", yarn: \"$YARN_VERSION\", berry: \"$BERRY_VERSION\", pnpm: \"$PNPM_VERSION\", bun: \"$BUN_VERSION\", deno: \"$DENO_VERSION\", nx: \"$NX_VERSION\", turbo: \"$TURBO_VERSION\", node: \"$NODE_VERSION\" }" > ./results/versions.json
