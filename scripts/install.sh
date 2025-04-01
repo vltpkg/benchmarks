a8a1a6ad9332bcd497443cde555adf1304efcc0d
darcyclarke <darcy@darcyclarke.me>
2025-04-01 16:19:52 -0400

    fix: runs flags

--- scripts/install.sh
+++ scripts/install.sh
@@ -24,7 +24,6 @@ echo "Running installation benchmark suite for $1..."
 hyperfine --export-json="../../results/$1/benchmarks.json" \
     --warmup 3 \
     --runs 10 \
-    --max-runs 15 \
     -i \
     --prepare 'bash ../../scripts/clean.sh' \
     -n 'npm' 'bash ../../scripts/install/npm.sh || true' \
