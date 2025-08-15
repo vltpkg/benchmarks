7bd6157f602e7f0c2261d8e6cb4a0ca063d2cc3f
darcyclarke <darcy@darcyclarke.me>
2025-08-15 12:49:00 -0400

    fix: amend paths

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -21,31 +21,31 @@ pushd "./fixtures/$1"
 # Run the install variation
 case "$2" in
     cache)
-        bash ../../scripts/variations/cache.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/cache.sh "../../scripts" "../../" "$1" "$2"
         ;;
     cache+lockfile)
-        bash ../../scripts/variations/cache+lockfile.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/cache+lockfile.sh "../../scripts" "../../" "$1" "$2"
         ;;
     cache+node_modules)
-        bash ../../scripts/variations/cache+node_modules.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/cache+node_modules.sh "../../scripts" "../../" "$1" "$2"
         ;;
     cache+lockfile+node_modules)
-        bash ../../scripts/variations/cache+lockfile+node_modules.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/cache+lockfile+node_modules.sh "../../scripts" "../../" "$1" "$2"
         ;;
     lockfile)
-        bash ../../scripts/variations/lockfile.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/lockfile.sh "../../scripts" "../../" "$1" "$2"
         ;;
     lockfile+node_modules)
-        bash ../../scripts/variations/lockfile+node_modules.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/lockfile+node_modules.sh "../../scripts" "../../" "$1" "$2"
         ;;
     node_modules)
-        bash ../../scripts/variations/node_modules.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/node_modules.sh "../../scripts" "../../" "$1" "$2"
         ;;
     clean)
-        bash ../../scripts/variations/clean.sh "../../scripts" "../" "$1" "$2"
+        bash ../../scripts/variations/clean.sh "../../scripts" "../../" "$1" "$2"
         ;;
     run)
-        bash ../../scripts/variations/run.sh "../../scripts" "../" "run" "run"
+        bash ../../scripts/variations/run.sh "../../scripts" "../../" "run" "run"
         ;;
     *)
         echo "Error: Unknown install variation '$2'"
