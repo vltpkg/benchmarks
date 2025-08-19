7b107cb12376d9d0b95726dc4d96a29171e19ac9
darcyclarke <darcy@darcyclarke.me>
2025-08-19 18:33:58 -0400

    fix: routing/path for assets

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/benchmarks/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
