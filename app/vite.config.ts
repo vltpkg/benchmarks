b6cae483731738944b4ff0e6880af9399cfa0d53
Jason Korol <ejkorol@gmail.com>
2025-08-22 18:44:31 -0400

    feat: updates the `benchmarks` routes (#23)
    - retires the `createHashRouter` in preference of `createBrowserRouter`
    - updates the `base` in `vite.config.ts` to root `/` instead of
      `/benchmarks/` (due to the cname)
    - updates the locations of `chart-data` and fixture variants

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/benchmarks/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
